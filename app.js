998169dee4266a24ee0a78b022180f2c43ee81e5
SohamPatel0810 <sohampatel2050@gmail.com>
2021-08-01 18:17:44 +0530

    updated

---
+++ app.js
@@ -0,0 +1,44 @@
+const billamount = document.querySelector("#bill-amt");
+const cashGiven = document.querySelector("#cash-given");
+const checkbutton = document.querySelector("#check-btn");
+const errormessage=document.querySelector("#error");
+const noOfNotes = document.querySelectorAll(".returnnotes");
+
+const availableNotes = [2000,500,200,100,50,20,10,5,2,1];
+
+checkbutton.addEventListener("click",inputvalidation);
+
+function inputvalidation(){
+    errormessage.style.display = "none";
+    if(billamount.value > 0)
+    {
+        if(Number(cashGiven.value) > Number(billamount.value))
+        {
+            const refund = cashGiven.value - billamount.value;
+            calculateNotes(refund);
+        }
+        else
+        {
+            errorhandler("Insufficient Cash ");
+        }
+    }
+    else
+    {
+        errorhandler("Invalid Bill Amount!!");
+    }
+}
+
+function errorhandler(text){
+    errormessage.style.display ='block';
+    errormessage.innerText = text;
+
+}
+function calculateNotes(refund){
+    for (let i=0; i< availableNotes.length; i++){
+        const notesreturned = Math.trunc(refund/availableNotes[i]);
+        refund %= availableNotes[i]
+        noOfNotes[i].innerText = notesreturned;
+
+    }
+
+}
